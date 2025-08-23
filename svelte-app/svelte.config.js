754a23babc064ca236f231bf5b2f2b848a201654
atomicdoubt <ariane.vartanian@gmail.com>
2025-08-23 15:50:01 -0700

    Update svelte.config.js

--- svelte-app/svelte.config.js
+++ svelte-app/svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
-export default {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: undefined,
-      precompress: false
-    })
-  }
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}
 };
+
+export default config;
