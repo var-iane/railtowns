02e1f286b93fdf515a336c46bbcf2ba534eb4983
atomicdoubt <ariane.vartanian@gmail.com>
2025-08-23 16:18:58 -0700

    Update +layout.svelte

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
 	import { page } from '$app/stores';
+	import { base } from '$app.paths';
 
   $: path = $page.url.pathname;
 
   $: theme =
-    path.startsWith('/1-gridtowns') ? 'night' :
-    path.startsWith('/2-railtowns') ? 'synthwave' :
-    path.startsWith('/3-ghosttowns') ? 'night' :
+    path.startsWith('${base}/1-gridtowns') ? 'night' :
+    path.startsWith('${base}/2-railtowns') ? 'synthwave' :
+    path.startsWith('${base}/3-ghosttowns') ? 'night' :
     'night';
 </script>
 
@@ -20,10 +21,10 @@
 <div data-theme={theme} class="min-h-screen">
 	<header class="navbar bg-base-200 border-b border-base-300 mb-4">
 	<div class="flex-1 px-4 text-xl font-bold">
-		<a href="/" class="btn btn-ghost normal-case text-xl">Intro</a>
-		<a href="/1-gridtowns" class="btn btn-soft btn-primary normal-case text-xl">Grid towns</a>
-		<a href="/2-railtowns" class="btn btn-soft btn-secondary normal-case text-xl">Rail towns</a>
-		<a href="/3-ghosttowns" class="btn btn-soft btn-neutral normal-case text-xl">Ghost towns</a>
+		<a href="${base}/" class="btn btn-ghost normal-case text-xl">Intro</a>
+		<a href="${base}/1-gridtowns" class="btn btn-soft btn-primary normal-case text-xl">Grid towns</a>
+		<a href="${base}/2-railtowns" class="btn btn-soft btn-secondary normal-case text-xl">Rail towns</a>
+		<a href="${base}/3-ghosttowns" class="btn btn-soft btn-neutral normal-case text-xl">Ghost towns</a>
 	</div>
 	</header>
 
